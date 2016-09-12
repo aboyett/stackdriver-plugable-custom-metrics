ce27e47ac607eda517eea65cfb283649e8c8991e
Andy Boyett <andy@cloudphysics.com>
2016-09-12 16:45:59 -0700

    add setup.py and requirements.txt
    this should allow the package to be installed via pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+#from setuptools import setup
+import uuid
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt", session=uuid.uuid1())
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='stackdriver-custom-metrics',
+    version='0.1.0-dev',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=reqs
+)
